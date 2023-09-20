fc8f109444dcc22b681ebd3fa2a0c80e910d8052
AfonsoAlemao <afonso.alemao01@gmail.com>
2023-09-20 19:14:20 +0100

    fan_speed_set

--- lab5/lab5_fan_speed.py
+++ lab5/lab5_fan_speed.py
@@ -17,3 +17,9 @@ C1 = sf.TriangleFuzzySet(0,0,1.5,   term="low")
 C2 = sf.TriangleFuzzySet(0.5,2,3.5,  term="normal")
 C3 = sf.TriangleFuzzySet(2.5,4,4, term="turbo")
 FS.add_linguistic_variable("clock_speed", sf.LinguisticVariable([C1, C2, C3], universe_of_discourse=[0,4]))
+
+
+
+F1 = sf.TriangleFuzzySet(0,0,3500,   term="low")
+F2 = sf.TriangleFuzzySet(2500,6000,6000,  term="normal")
+FS.add_linguistic_variable("fan_speed", sf.LinguisticVariable([F1, F2], universe_of_discourse=[0,6000]))
