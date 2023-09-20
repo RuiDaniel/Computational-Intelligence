1e47adbe534cc0f0edf6d89c5a97b480d12ff04c
AfonsoAlemao <afonso.alemao01@gmail.com>
2023-09-20 19:06:27 +0100

    core_temperature_set

--- lab5/lab5_fan_speed.py
+++ lab5/lab5_fan_speed.py
@@ -5,3 +5,11 @@ import simpful as sf
 # – Clock speed (in GHz)
 
 FS = sf.FuzzySystem()
+
+# Determine Fuzzy Sets
+
+T1 = sf.TriangleFuzzySet(0,0,50,   term="cold")
+T2 = sf.TriangleFuzzySet(30,50,70,  term="warm")
+T3 = sf.TriangleFuzzySet(50,100,100, term="hot")
+FS.add_linguistic_variable("core_temperature", sf.LinguisticVariable([T1, T2, T3], universe_of_discourse=[0,100]))
+
