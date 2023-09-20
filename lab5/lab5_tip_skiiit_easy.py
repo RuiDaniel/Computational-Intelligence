563f1459e7560375c0262060bd673d390dbf4b9a
AfonsoAlemao <afonso.alemao01@gmail.com>
2023-09-20 21:52:25 +0100

    default terms

--- lab5/lab5_tip_skiiit_easy.py
+++ lab5/lab5_tip_skiiit_easy.py
@@ -9,7 +9,7 @@ service = ctrl.Antecedent(np.arange(0, 11, 1), 'service')
 tip = ctrl.Consequent(np.arange(0, 26, 1), 'tip')
 
 # Auto-membership function population is possible with .automf(3, 5, or 7)
-quality.automf(3)
+quality.automf(3) # 3 terms para a variável quality -> ex: (poor, average, good) -> default terms 
 service.automf(3)
 
 # Custom membership functions can be built interactively with a familiar,
