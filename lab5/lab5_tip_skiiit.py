85eb293c9c7df2240224a4c58aab6ff832a9a3c1
AfonsoAlemao <afonso.alemao01@gmail.com>
2023-09-20 21:44:40 +0100

    Defuzzification

--- lab5/lab5_tip_skiiit.py
+++ lab5/lab5_tip_skiiit.py
@@ -104,3 +104,32 @@ plt.tight_layout()
 
 
 plt.show()
+
+# Aggregate all three output membership functions together
+aggregated = np.fmax(tip_activation_lo,
+                     np.fmax(tip_activation_md, tip_activation_hi))
+
+# Calculate defuzzified result
+tip = fuzz.defuzz(x_tip, aggregated, 'centroid')
+tip_activation = fuzz.interp_membership(x_tip, aggregated, tip)  # for plot
+
+# Visualize this
+fig, ax0 = plt.subplots(figsize=(8, 3))
+
+ax0.plot(x_tip, tip_lo, 'b', linewidth=0.5, linestyle='--', )
+ax0.plot(x_tip, tip_md, 'g', linewidth=0.5, linestyle='--')
+ax0.plot(x_tip, tip_hi, 'r', linewidth=0.5, linestyle='--')
+ax0.fill_between(x_tip, tip0, aggregated, facecolor='Orange', alpha=0.7)
+ax0.plot([tip, tip], [0, tip_activation], 'k', linewidth=1.5, alpha=0.9)
+ax0.set_title('Aggregated membership and result (line)')
+
+# Turn off top/right axes
+for ax in (ax0,):
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.get_xaxis().tick_bottom()
+    ax.get_yaxis().tick_left()
+
+plt.tight_layout()
+
+plt.show()
